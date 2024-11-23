cf8d1581c8be285300d93b824a375fd0372954d0
Teodor Golisharski <t.golisharski@abv.bg>
2024-11-24 00:11:22 +0200

    Code format on TradeCommissions

--- Lab-Conditional-Statements-Advanced/src/TradeCommissions.java
+++ Lab-Conditional-Statements-Advanced/src/TradeCommissions.java
@@ -11,28 +11,27 @@ public class TradeCommissions {
 
         switch (town) {
             case "Sofia" -> {
-                if(sum > 10000) coefficient = 0.12;
-                else if(sum > 1000) coefficient = 0.08;
-                else if(sum > 500) coefficient = 0.07;
-                else if(sum >= 0) coefficient = 0.05;
+                if (sum > 10000) coefficient = 0.12;
+                else if (sum > 1000) coefficient = 0.08;
+                else if (sum > 500) coefficient = 0.07;
+                else if (sum >= 0) coefficient = 0.05;
             }
             case "Varna" -> {
-                if(sum > 10000) coefficient = 0.13;
-                else if(sum > 1000) coefficient = 0.10;
-                else if(sum > 500) coefficient = 0.075;
-                else if(sum >= 0) coefficient = 0.045;
+                if (sum > 10000) coefficient = 0.13;
+                else if (sum > 1000) coefficient = 0.10;
+                else if (sum > 500) coefficient = 0.075;
+                else if (sum >= 0) coefficient = 0.045;
             }
             case "Plovdiv" -> {
-                if(sum > 10000) coefficient = 0.145;
-                else if(sum > 1000) coefficient = 0.12;
-                else if(sum > 500) coefficient = 0.08;
-                else if(sum >= 0) coefficient = 0.055;
+                if (sum > 10000) coefficient = 0.145;
+                else if (sum > 1000) coefficient = 0.12;
+                else if (sum > 500) coefficient = 0.08;
+                else if (sum >= 0) coefficient = 0.055;
             }
         }
-        if(coefficient == 0){
+        if (coefficient == 0) {
             System.out.println("error");
-        }
-        else{
+        } else {
             System.out.printf("%.2f", sum * coefficient);
         }
     }
