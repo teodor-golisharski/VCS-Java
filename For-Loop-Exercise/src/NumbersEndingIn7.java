c27b9d793268251c21b0dc03a0a3de4a45db3ceb
Teodor Golisharski <t.golisharski@abv.bg>
2024-11-28 21:13:13 +0200

    For-Loop-Exercise created and first task completed

---
+++ For-Loop-Exercise/src/NumbersEndingIn7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NumbersEndingIn7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(7);
+        for (int i = 1; i <= 99; i++) {
+            System.out.println(i + "7");
+        }
+    }
+}
