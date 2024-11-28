cfb83e4ff511ecf12b07f3b84b04dad9b4cdd5f8
Teodor Golisharski <t.golisharski@abv.bg>
2024-11-28 21:19:27 +0200

    HalfSumElement added.

---
+++ For-Loop-Exercise/src/HalfSumElement.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class HalfSumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            sum += number;
+
+            if (number > max) {
+                max = number;
+            }
+        }
+
+        int sumWithOutMaxNumber = sum - max;
+
+        if (max == sumWithOutMaxNumber) {
+            System.out.println("Yes");
+            System.out.println("Sum = " + max);
+        }
+        else{
+            int diff = Math.abs(max - sumWithOutMaxNumber);
+            System.out.println("No");
+            System.out.println("Diff = " + diff);
+        }
+    }
+}
