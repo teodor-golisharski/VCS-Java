8c2983e37d34dbb5a3d135e03e8c3666aa930018
Teodor Golisharski <t.golisharski@abv.bg>
2024-11-29 17:39:34 +0200

    TennisRanklist added.

---
+++ For-Loop-Exercise/src/TennisRanklist.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TennisRanklist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int startingPoints = Integer.parseInt(scanner.nextLine());
+
+        int pointsAcquired = startingPoints;
+
+        double count = 0;
+        for (int i = 0; i < n; i++) {
+            String state = scanner.nextLine();
+
+            switch (state) {
+                case "W" -> {
+                    pointsAcquired += 2000;
+                    count++;
+                }
+                case "F" -> {
+                    pointsAcquired += 1200;
+                }
+                case "SF" -> {
+                    pointsAcquired += 720;
+                }
+            }
+        }
+        int temp = (int)Math.floor((double)((pointsAcquired - startingPoints) / n));
+        System.out.printf("Final points: %d%n", pointsAcquired);
+        System.out.printf("Average points: %d%n", temp);
+        System.out.printf("%.2f%%%n", count / n * 100);
+    }
+}
