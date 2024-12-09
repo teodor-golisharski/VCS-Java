48685b7082de70da1b941656a934fd053794cfd8
Teodor Golisharski <t.golisharski@abv.bg>
2024-12-09 20:30:55 +0200

    Travelling task solution completed.

--- Lab-Nested-Loops/src/Travelling.java
+++ Lab-Nested-Loops/src/Travelling.java
@@ -4,6 +4,19 @@ public class Travelling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String input = scanner.nextLine();
+        while (!input.equals("End")) {
+            double budgetNeeded = Double.parseDouble(scanner.nextLine());
+            double sum = 0;
+            while (true) {
+                double temp = Double.parseDouble(scanner.nextLine());
+                sum += temp;
+                if (sum >= budgetNeeded) {
+                    System.out.printf("Going to %s!%n", input);
+                    break;
+                }
+            }
+            input = scanner.nextLine();
+        }
     }
 }
